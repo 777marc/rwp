4740f88fec5ca3b1b348729ee8ef4d26a2d31293
777marc@gmail.com <777marc@gmail.com>
2021-03-03 19:21:26 -0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 console.log('main js');
+
+function generatePlan() {
+
+    // intensity: 0 = easy, 1 = medium, 2 = hard
+    // long run day: 0 - 6 (mon - sun)
+    let numberWeeks = 12;
+    let intensity = 1;
+    let numberDays = 4;
+    let longRunDay = 6;
+
+    let weeks = [];
+
+    for (i = 1; i <= numberWeeks; i++) {
+        let week = [];
+        for (w = 1; w <= numberDays; w++) {
+            let day = {
+                week : i,
+                day : w,
+                miles : 3
+            };
+            week.push(day);
+        }
+        weeks.push(week); 
+    }
+    console.log(weeks)
+}
