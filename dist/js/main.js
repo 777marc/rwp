2fefa57a600990f782b4896e454c34f7b0504902
777marc@gmail.com <777marc@gmail.com>
2021-03-06 14:10:17 -0500

    misc

--- dist/js/main.js
+++ dist/js/main.js
@@ -24,4 +24,29 @@ function generatePlan() {
         weeks.push(week); 
     }
     console.log(weeks)
+    tableCreate(weeks);
 }
+
+function tableCreate(weeks) {
+    var body = document.getElementById('results-tbl');
+    var tbl = document.createElement('table');
+    tbl.style.width = '100%';
+    tbl.setAttribute('border', '1');
+    var tbdy = document.createElement('tbody');
+    for (var i = 0; i < weeks.length; i++) {
+      var tr = document.createElement('tr');
+      for (var j = 0; j < 2; j++) {
+        if (i == 2 && j == 1) {
+          break
+        } else {
+          var td = document.createElement('td');
+          td.appendChild(document.createTextNode('hello'))
+          i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+          tr.appendChild(td)
+        }
+      }
+      tbdy.appendChild(tr);
+    }
+    tbl.appendChild(tbdy);
+    body.appendChild(tbl)
+  }
